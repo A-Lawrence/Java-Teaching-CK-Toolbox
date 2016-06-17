af5d0f4c61a914883aaee75e700235072bdd089a
connorkeevill <connor.keevill.19@gmail.com>
2016-06-17 10:58:33 +0100

    Added NameInputAssignment1

---
+++ src/NameInputAssignment1.java
@@ -0,0 +1,32 @@
+/**
+ * Created by connorkeevill12 on 17/06/2016.
+ */
+public class NameInputAssignment1 {
+
+    public static void main(String[] args) {
+        String currentName = null;
+
+        while (true) {
+            currentName = Helpers.getInput("Enter a name, or enter 'Quit' to end the progam: ");
+
+            guardAgainstQuitValue(currentName);
+
+            System.out.println("Hello " + capitaliseFirstLetter(currentName));
+        }
+    }
+
+    private static void guardAgainstQuitValue(String givenValue){
+        if(givenValue.equalsIgnoreCase("QUIT")){
+            System.exit(0);
+        }
+    }
+
+    public static String capitaliseFirstLetter(String message) {
+        message = message.toLowerCase();
+
+        String firstLetter = message.substring(0, 1).toUpperCase();
+        String restOfString = message.substring(1);
+
+        return firstLetter + restOfString;
+    }
+}
