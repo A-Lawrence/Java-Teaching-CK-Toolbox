80931491d22fc0b012e2547932be81b812171b0d
connorkeevill <connor.keevill.19@gmail.com>
2016-06-19 17:58:04 +0100

    Added DataTypesPractice5 and updated Helpers

--- src/Helpers.java
+++ src/Helpers.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.jar.Pack200;
 
 /**
  * Created by connorkeevill12 on 13/06/2016.
@@ -15,6 +16,30 @@ public class Helpers {
         String r = getInput(message);
 
         return Integer.parseInt(r);
+    }
+    public static int getIntRange(int lowest, int highest, String prompt){
+        int number;
+
+        do {
+            number = getIntput(prompt);
+        } while(number < lowest || number > highest);
+
+        return number;
+    }
+    public static double getDouble(String prompt){
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.print(prompt);
+
+        return myScanner.nextDouble();
+    }
+
+    public static String getConformation(String prompt, String expectedOne, String expectedTwo){
+        String confirm;
+        do {
+            confirm = getInput(prompt + "(" + expectedOne + "/" + expectedTwo + ")");
+        } while (!confirm.equalsIgnoreCase(expectedOne) && !confirm.equalsIgnoreCase(expectedTwo));
+        return confirm;
     }
 }
 
